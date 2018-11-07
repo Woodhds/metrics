8eb75e08f4aeeec3cfca71b6501664148494f69f
borodinsv <borodinsv@npc.ba>
2018-11-07 19:23:17 +0300

    repostOne

--- metrics/ClientApp/src/app/user/user.component.ts
+++ metrics/ClientApp/src/app/user/user.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import {State} from '@progress/kendo-data-query';
 import {DataStateChangeEvent, GridDataResult} from '@progress/kendo-angular-grid';
-import { UserService } from '../services/user.service';
+import {UserService} from '../services/user.service';
 import {VkMessage, VkRepostModel, VkUser} from "./VkResponse";
 import {NotificationService} from "@progress/kendo-angular-notification";
 
@@ -15,7 +15,7 @@ export class UserComponent implements OnInit {
   public data: GridDataResult;
   public users: VkUser[] = [];
   public selected: VkMessage[] = [];
-  public userId: {FullName: '', UserId: ''};
+  public userId: { FullName: '', UserId: '' };
   public search = '';
   public timeout: number = 15;
   public loading = false;
@@ -24,7 +24,9 @@ export class UserComponent implements OnInit {
     skip: 0,
     take: 100
   };
-  constructor(private userService: UserService, private notificationService: NotificationService) {}
+
+  constructor(private userService: UserService, private notificationService: NotificationService) {
+  }
 
   ngOnInit() {
     this.userService.getUsers().subscribe(data => this.users = data.data);
@@ -46,18 +48,30 @@ export class UserComponent implements OnInit {
     this.handleSearch();
   }
 
-  repost(repost: VkRepostModel[], element: HTMLElement) {
+  repostOne(repost: VkRepostModel[], element: HTMLElement) {
+    this.repost(repost, 0, () => {
+      element.classList.remove('k-i-fav-outline');
+      element.classList.add('text-danger');
+      element.classList.add('k-i-fav');
+    })
+  }
+
+  repostAll() {
+  }
+
+  repost(repost: VkRepostModel[], timeout: number, callback: Function) {
     this.loading = true;
     this.userService.repost(repost).subscribe(z => {
       if (z) {
         this.notificationService.show(
-          {content: 'Выполнено успешно',
-            position: { vertical: 'bottom', horizontal: 'right' },
-            type: { style: 'success', icon: true }
+          {
+            content: 'Выполнено успешно',
+            position: {vertical: 'bottom', horizontal: 'right'},
+            type: {style: 'success', icon: true}
           });
-        element.classList.remove('k-i-fav-outline');
-        element.classList.add('text-danger');
-        element.classList.add('k-i-fav');
+        if (callback) {
+          callback();
+        }
         this.loading = true;
       }
     });
