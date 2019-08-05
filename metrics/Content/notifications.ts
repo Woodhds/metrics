6191a7089467a7c3c08595c6614772c112b50aa5
s.borodin <borodinsv@npc.ba>
2019-08-05 15:20:33 +0300

    notifications in header

--- metrics/Content/notifications.ts
+++ metrics/Content/notifications.ts
@@ -2,8 +2,21 @@ import * as signalR from '@aspnet/signalr';
 
 const connection = new signalR.HubConnectionBuilder().withUrl('/notifications').build();
 
-connection.on('count', args => {
-  console.log(args)
+connection.on('count', (count: number) => {
+  let el = document.getElementById('actions-count');
+
+  if (el != null) {
+    let countTag = el.getElementsByClassName('count');
+    if (countTag.length > 0) {
+      if (count != 0) {
+        countTag[0].innerHTML = `${count}`;
+        el.classList.remove('hidden')
+      } else {
+        el.classList.add('hidden');
+        countTag[0].innerHTML = '';
+      }
+    }
+  }
 });
 
-connection.start().catch(err => console.log(err))
+connection.start().catch(err => console.log(err));
