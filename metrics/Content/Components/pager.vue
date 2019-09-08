2b7b24ca2ac02f6ee62245a08f8882ef24405af3
s.borodin <woodhds@gmail.com>
2019-09-08 11:56:38 +0300

    refactor

---
+++ metrics/Content/Components/pager.vue
@@ -0,0 +1,27 @@
+<template>
+  <ul class="flex mt-6 flex-wrap" v-if="totalPages.length > 1">
+    <li
+      class="px-4 py-2 cursor-pointer"
+      @click="onChangePage(item)"
+      :key="item"
+      :class="[ item === page ? 'bg-blue-800 text-white': '' ]"
+      v-for="item of totalPages"
+    >{{item}}</li>
+  </ul>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Component, Prop, Emit } from "vue-property-decorator";
+
+@Component
+export default class PagerComponent extends Vue {
+  @Prop() totalPages: number = 1;
+  @Prop() page: number = 1;
+
+  @Emit("changePage")
+  onChangePage(t: number): number {
+    return t;
+  }
+}
+</script>
--- metrics/Content/Components/usercomponent.vue
+++ metrics/Content/Components/usercomponent.vue
@@ -1,167 +1,208 @@
 <template>
   <div class="px-5 py-5 flex flex-col relative flex-wrap">
-    <SwitchComponent class="mb-3" @switchChange="switchChange" onText="Сайт" offText="Юзер"
-                     :value="switchFromUser"></SwitchComponent>
+    <SwitchComponent
+      class="mb-3"
+      @switchChange="switchChange"
+      onText="Сайт"
+      offText="Юзер"
+      :value="switchFromUser"
+    ></SwitchComponent>
     <div class="flex flex-row mb-5 flex-wrap justify-between">
-      <form v-if="!switchFromUser" @submit.prevent="searchMessages"
-            class="rounded px-5 py-5 shadow-md sm:w-full md:w-1/2 sm:mb-4">
-        <Dropdown Label="Пользователь" :List="users" @select="handleSelect"></Dropdown>
+      <form
+        v-if="!switchFromUser"
+        @submit.prevent="searchMessages"
+        class="rounded px-5 py-5 shadow-md sm:w-full md:w-1/2 sm:mb-4"
+      >
+        <Dropdown label="Пользователь" :List="users" @select="handleSelect"></Dropdown>
         <div class="mb-4 flex flex-col">
           <label class="text-sm font-bold text-gray-900 block" for="search">Поиск</label>
-          <input id="search" name="search"
-                 class="shadow px-5 py-2 leading-tight appearance-none rounded focus:outline-none focus:shadow-outline"
-                 v-model.trim="search"/>
+          <input
+            id="search"
+            name="search"
+            class="shadow px-5 py-2 leading-tight appearance-none rounded focus:outline-none focus:shadow-outline"
+            v-model.trim="search"
+          />
         </div>
-        <button type="submit" class="appearance-none bg-gray-800 hover:bg-gray-900 text-white py-2 px-5 rounded"
-                :disabled="!selected">Поиск
-        </button>
+        <button
+          type="submit"
+          class="appearance-none bg-gray-800 hover:bg-gray-900 text-white py-2 px-5 rounded"
+          :disabled="!selected"
+        >Поиск</button>
       </form>
-      <div class="sm:w-full rounded px-5 py-5 shadow-md md:w-1/3 flex flex-col"
-           v-if="selectedMess.length > 0">
+      <div
+        class="sm:w-full rounded px-5 py-5 shadow-md md:w-1/3 flex flex-col"
+        v-if="selectedMess.length > 0"
+      >
         <div class="mb-4 relative">
           <label for="timeout" class="text-sm font-bold text-gray-800 block">Таймаут с сек.</label>
-          <select id="timeout"
-                  class="block appearance-none w-full bg-white border border-gray-100 hover:border-gray-300 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
-                  v-model="timeout">
+          <select
+            id="timeout"
+            class="block appearance-none w-full bg-white border border-gray-100 hover:border-gray-300 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+            v-model="timeout"
+          >
             <option v-for="second of seconds" :value="second" :key="second">{{second}}</option>
           </select>
-          <div class="pointer-events-none dropdown-list__arrow absolute right-0 flex items-center px-2 text-gray-900">
-            <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-              <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/>
+          <div
+            class="pointer-events-none dropdown-list__arrow absolute right-0 flex items-center px-2 text-gray-900"
+          >
+            <svg
+              class="fill-current h-4 w-4"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
             </svg>
           </div>
         </div>
-        <button @click="repostAll"
-                class="appearance-none bg-blue-800 hover:bg-blue-900 text-white py-2 px-5 rounded">
-          Репост всего
-        </button>
+        <button
+          @click="repostAll"
+          class="appearance-none bg-blue-800 hover:bg-blue-900 text-white py-2 px-5 rounded"
+        >Репост всего</button>
       </div>
     </div>
     <div v-if="messages.length > 0" class="flex flex-col">
       <div class="flex flex-row flex-wrap">
-        <Message v-for="message of messages" @select="onSelect" :message="message"
-                 :key="message.Id + '_' + message.Owner_Id">
-        </Message>
+        <Message
+          v-for="message of messages"
+          @select="onSelect"
+          :message="message"
+          :key="message.Id + '_' + message.Owner_Id"
+        ></Message>
       </div>
-      <ul class="flex mt-6 flex-wrap" v-if="totalPages.length > 1">
-        <li class="px-4 py-2 cursor-pointer" @click="page = item" :key="item"
-            :class="[ item === page ? 'bg-blue-800 text-white': '' ]" v-for="item of totalPages">{{item}}
-        </li>
-      </ul>
+      <PagerComponent :page="page" :total-pages="totalPages" @changePage="onChangePage"></PagerComponent>
     </div>
     <div v-show="isLoading" class="loading"></div>
   </div>
 </template>
 
 <script lang="ts">
-  import {Component, Watch} from 'vue-property-decorator';
-  import axios from 'axios';
-  import Vue from 'vue';
-  import {VkUser} from "../models/user";
-  import Dropdown from "./dropdown.vue";
-  import {VkMessage, VkRepostModel} from "../models/VkMessage";
-  import Message from "./message.vue";
-  import {searchMessages, repost, getFromSite} from '../services/MessageService';
-  import {SelectMessageModel} from "../models/SelectMessageModel";
-  import SwitchComponent from "./switch.vue";
+import { Component, Watch } from "vue-property-decorator";
+import axios from "axios";
+import Vue from "vue";
+import { VkUser } from "../models/user";
+import Dropdown from "./dropdown.vue";
+import { VkMessage, VkRepostModel } from "../models/VkMessage";
+import Message from "./message.vue";
+import {
+  searchMessages,
+  repost,
+  getFromSite
+} from "../services/MessageService";
+import { SelectMessageModel } from "../models/SelectMessageModel";
+import SwitchComponent from "./switch.vue";
+import PagerComponent from "./pager.vue";
 
-  @Component({
-    components: {Dropdown, Message, SwitchComponent}
-  })
-  export default class UserComponent extends Vue {
-    users: VkUser[] = [];
-    search: string = '';
-    selected: VkUser | null = null;
-    messages: VkMessage[] = [];
-    isLoading: boolean = false;
-    pageSize: number = 100;
-    page: number = 1;
-    total: number = 0;
-    timeout: number = 40;
-    selectedMess: SelectMessageModel[] = [];
-    switchFromUser: boolean = false;
+@Component({
+  components: { Dropdown, Message, SwitchComponent, PagerComponent }
+})
+export default class UserComponent extends Vue {
+  users: VkUser[] = [];
+  search: string = "";
+  selected: VkUser | null = null;
+  messages: VkMessage[] = [];
+  isLoading: boolean = false;
+  pageSize: number = 100;
+  page: number = 1;
+  total: number = 0;
+  timeout: number = 40;
+  selectedMess: SelectMessageModel[] = [];
+  switchFromUser: boolean = false;
 
-    @Watch('page')
-    pageChange() {
-      this.searchMessages();
-      window.scroll({top: 0, behavior: "smooth"});
-    }
+  @Watch("page")
+  pageChange() {
+    this.searchMessages();
+    window.scroll({ top: 0, behavior: "smooth" });
+  }
 
-    @Watch('switchFromUser')
-    fetchFromSite() {
-      if (this.switchFromUser) {
-        getFromSite().then(response => {
-          this.messages = response.data.Data;
-          this.total = response.data.Total;
-        })
-      }
-    }
-
-    beforeMount(): void {
-      axios.get<VkUser[]>('/user/users').then(response => {
-        this.users = response.data;
-      });
-    }
-
-    switchChange(value: boolean): void {
-      this.switchFromUser = value;
-    }
-
-    showLoading() {
-      this.isLoading = true;
-    }
-
-    hideLoading() {
-      this.isLoading = false;
-    }
-
-    searchMessages(): void {
-      this.showLoading();
-      searchMessages(this.search, this.selected != null ? this.selected.UserId : 0, this.page, this.pageSize)
-        .then(response => {
-          this.messages = response.data.Data;
-          this.total = response.data.Total;
-        }).then(() => (this.hideLoading()));
-    }
-
-    handleSelect(user: VkUser): void {
-      this.selected = user;
-    }
-
-    get totalPages(): number[] {
-      let arr = [];
-      for (let i = 1; i <= Math.ceil(this.total / this.pageSize); i++) {
-        arr.push(i)
-      }
-
-      return arr;
-    }
-
-    get seconds(): number[] {
-      let arr = [];
-      for (let i = 30; i <= 60; i++) {
-        arr.push(i)
-      }
-      return arr;
-    }
-
-    onSelect(model: SelectMessageModel) {
-      let idx = this.selectedMess.findIndex(d => d.Owner_Id == model.Owner_Id && d.Id == model.Id);
-      if (!model.IsSelect && idx !== -1) {
-          this.selectedMess.splice(idx, 1);
-      } else {
-        this.selectedMess.push(model)
-      }
-    }
-
-    repostAll() {
-      this.showLoading();
-      let mess = this.messages.filter(d => d.IsSelected);
-      const reposts = mess.map(e => new VkRepostModel(e.Owner_Id, e.Id));
-      repost(reposts, this.timeout).then().then(d => {
-        this.hideLoading();
-        mess.forEach(e => e.IsSelected = false);
+  @Watch("switchFromUser")
+  fetchFromSite() {
+    if (this.switchFromUser) {
+      getFromSite().then(response => {
+        this.messages = response.data.Data;
+        this.total = response.data.Total;
       });
     }
   }
+
+  beforeMount(): void {
+    axios.get<VkUser[]>("/user/users").then(response => {
+      this.users = response.data;
+    });
+  }
+
+  switchChange(value: boolean): void {
+    this.switchFromUser = value;
+  }
+
+  showLoading() {
+    this.isLoading = true;
+  }
+
+  hideLoading() {
+    this.isLoading = false;
+  }
+
+  searchMessages(): void {
+    this.showLoading();
+    searchMessages(
+      this.search,
+      this.selected != null ? this.selected.UserId : 0,
+      this.page,
+      this.pageSize
+    )
+      .then(response => {
+        this.messages = response.data.Data;
+        this.total = response.data.Total;
+      })
+      .then(() => this.hideLoading());
+  }
+
+  handleSelect(user: VkUser): void {
+    this.selected = user;
+  }
+
+  get totalPages(): number[] {
+    let arr = [];
+    for (let i = 1; i <= Math.ceil(this.total / this.pageSize); i++) {
+      arr.push(i);
+    }
+
+    return arr;
+  }
+
+  get seconds(): number[] {
+    let arr = [];
+    for (let i = 30; i <= 60; i++) {
+      arr.push(i);
+    }
+    return arr;
+  }
+
+  onSelect(model: SelectMessageModel) {
+    let idx = this.selectedMess.findIndex(
+      d => d.Owner_Id == model.Owner_Id && d.Id == model.Id
+    );
+    if (!model.IsSelect && idx !== -1) {
+      this.selectedMess.splice(idx, 1);
+    } else {
+      this.selectedMess.push(model);
+    }
+  }
+
+  onChangePage(page: number): void {
+    this.page = page;
+  }
+
+  repostAll() {
+    this.showLoading();
+    let mess = this.messages.filter(d => d.IsSelected);
+    const reposts = mess.map(e => new VkRepostModel(e.Owner_Id, e.Id));
+    repost(reposts, this.timeout)
+      .then()
+      .then(d => {
+        this.hideLoading();
+        mess.forEach(e => (e.IsSelected = false));
+      });
+  }
+}
 </script>
