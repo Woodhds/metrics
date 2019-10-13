d99ad03700f5d50ae5e91fba196aafae79115472
borodinsv <woodhds@gmail.com>
2019-10-13 12:37:10 +0300

    up packages

--- metrics/postcss.config.js
+++ metrics/postcss.config.js
@@ -1,6 +1,29 @@
+
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[\w-/:]+(?<!:)/g) || []
+  }
+}
+
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: [
+    './Content/Components/**/*.vue',
+    './views/**/*.cshtml'
+  ],
+  extractors: [{
+    extractor: TailwindExtractor,
+    extensions: ['html', 'js', 'cshtml', 'vue']
+  }]
+})
+
+
 module.exports = {
+ 
   plugins: [
     require('tailwindcss')('./Content/tailwind.config.js'),
-    require('autoprefixer')
+    require('autoprefixer'),
+    ...process.env.NODE_ENV === 'production'
+      ? [purgecss]
+      : []
   ]
 };
--- metrics/webpack.production.js
+++ metrics/webpack.production.js
@@ -2,16 +2,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const merge = require('webpack-merge');
 const base = require('./webpack.base');
 const terserJs = require('terser-webpack-plugin');
-const purgecss = require('purgecss-webpack-plugin');
-const glob = require('glob-all');
-const path = require('path');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-
-class TailwindExtractor {
-  static extract(content) {
-    return content.match(/[A-Za-z0-9-_:\/]+/g) || []
-  }
-}
 
 module.exports = merge(base, {
   mode: 'production',
@@ -24,16 +15,6 @@ module.exports = merge(base, {
   plugins: [
     new MiniCssExtractPlugin({
       filename: './css/[name].css'
-    }),
-    new purgecss({
-      paths: glob.sync([
-        path.join(__dirname, './Content/Components/**/*.vue'), 
-        path.join(__dirname, './views/**/*.cshtml')
-      ]),
-      extractors: [{
-        extractor: TailwindExtractor,
-        extensions: ['html', 'js', 'cshtml', 'vue']
-      }]
     })
   ]
 });
