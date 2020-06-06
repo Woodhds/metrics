33baf0d9e88c7dd10ee5a4b44276ee31944a44f3
borodinsv <borodinsv@npc.ba>
2020-06-06 16:31:31 +0300

    up

--- metrics.Gateway/Startup.cs
+++ metrics.Gateway/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Ocelot.Provider.Consul;
 
 namespace metrics.Gateway
 {
@@ -17,13 +18,12 @@ namespace metrics.Gateway
         {
             _configuration = configuration;
         }
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMetricsAuthentication(_configuration);
 
-            services.AddOcelot();
+            services.AddOcelot().AddConsul();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
