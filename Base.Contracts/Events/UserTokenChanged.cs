ba18a638fdf873d3277f99fb93c51ebf282d3df6
borodinsv <borodinsv@itwebnet.ru>
2020-10-05 07:00:00 +0300

    more protos

--- Base.Contracts/Events/UserTokenChanged.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Base.Contracts.Events
-{
-    public class UserTokenChanged
-    {
-        public string Token { get; set; } = "";
-        public int UserId { get; set; }
-    }
-}
--- Base.Contracts/Events/UserTokenRemoved.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Base.Contracts.Events
-{
-    public class UserTokenRemoved
-    {
-        public int UserId { get; set; }
-        public string Name { get; set; } = "";
-    }
-}
