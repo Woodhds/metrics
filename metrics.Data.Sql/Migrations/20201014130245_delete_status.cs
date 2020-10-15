53e272cbc94d928aba89c6f9fd227fd2bdd5e2d9
borodinsv <borodinsv@npc.ba>
2020-10-15 16:56:45 +0300

    +

---
+++ metrics.Data.Sql/Migrations/20201014130245_delete_status.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace metrics.Data.Sql.Migrations
+{
+    public partial class delete_status : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "MessageVk");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "VkUserModel",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer")
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "VkUserModel",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer")
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "MessageVk",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
