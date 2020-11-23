22c2a95e44b96d03780a872131a7142ddf06a56d
borodinsv <borodinsv@npc.ba>
2020-11-23 15:19:59 +0300

    +

--- metrics.Frontend/src/app/modules/repost/components/repost/repost.component.spec.ts
+++ metrics.Frontend/src/app/modules/repost/components/repost/repost.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { RepostComponent } from './repost.component';
 
@@ -6,7 +6,7 @@ describe('RepostComponent', () => {
   let component: RepostComponent;
   let fixture: ComponentFixture<RepostComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ RepostComponent ]
     })
